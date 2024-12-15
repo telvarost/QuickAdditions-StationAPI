07a5794102dcd7b8fd3f705cca507a8e5163a243
telvarost <telontelvarost@gmail.com>
2024-12-15 00:01:25 -0500

    Lower portal volume

--- src/main/java/com/github/telvarost/quickadditions/mixin/SoundHelperMixin.java
+++ src/main/java/com/github/telvarost/quickadditions/mixin/SoundHelperMixin.java
@@ -1,6 +1,8 @@
 package com.github.telvarost.quickadditions.mixin;
 
 import com.github.telvarost.quickadditions.Config;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.sound.Sound;
@@ -11,11 +13,10 @@ import net.minecraft.world.biome.Biome;
 import net.modificationstation.stationapi.api.entity.player.PlayerHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import paulscode.sound.SoundSystem;
 
 import java.io.File;
 import java.util.Random;
@@ -196,5 +197,43 @@ public abstract class SoundHelperMixin {
     )
     private int quickAdditions_handleBackgroundMusicRandomIntervalMin(int constant) {
         return Config.config.MUSIC_CONFIG.musicCoundownRandomIntervalMin;
+    }
+
+    @WrapOperation(
+            method = "playSound(Ljava/lang/String;FF)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lpaulscode/sound/SoundSystem;setVolume(Ljava/lang/String;F)V"
+            )
+    )
+    public void quickAdditions_playSoundPositional(
+            SoundSystem instance, String f, float v, Operation<Void> original,
+            String id, float volume, float pitch
+    ) {
+        if (id.startsWith("portal")) {
+            System.out.println("Lowering portal volume");
+            original.call(instance, f, (v / 10.0f));
+        } else {
+            original.call(instance, f, v);
+        }
+    }
+
+    @WrapOperation(
+            method = "playSound(Ljava/lang/String;FFFFF)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lpaulscode/sound/SoundSystem;setVolume(Ljava/lang/String;F)V"
+            )
+    )
+    public void quickAdditions_playSound(
+            SoundSystem instance, String f, float v, Operation<Void> original,
+            String id, float x, float y, float z, float volume, float pitch
+    ) {
+        if (id.startsWith("portal")) {
+            System.out.println("Lowering portal volume2");
+            original.call(instance, f, (v / 10.0f));
+        } else {
+            original.call(instance, f, v);
+        }
     }
 }
