6ff314954b452bb75903b87443fe2664a51fd209
telvarost <telontelvarost@gmail.com>
2024-12-18 18:22:35 -0500

    Make it so that dimension specific music only plays in its dimension

--- src/main/java/com/github/telvarost/quickadditions/ModHelper.java
+++ src/main/java/com/github/telvarost/quickadditions/ModHelper.java
@@ -3,5 +3,7 @@ package com.github.telvarost.quickadditions;
 public class ModHelper {
 
     public static class ModHelperFields {
+        public static int songLevelId = Integer.MAX_VALUE;
+        public static boolean cancelCurrentBGM = false;
     }
 }
